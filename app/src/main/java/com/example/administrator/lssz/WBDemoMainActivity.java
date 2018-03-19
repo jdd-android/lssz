8dfca479384722af88991c7682f0adbd3ed2af64
yuqing <yuqing@jiangduoduo.com>
2018-03-19 17:10:16 +0800

    userMessage

--- app/src/main/java/com/example/administrator/lssz/WBDemoMainActivity.java
+++ app/src/main/java/com/example/administrator/lssz/WBDemoMainActivity.java
@@ -3,27 +3,33 @@ package com.example.administrator.lssz;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sina.weibo.sdk.WbSdk;
-import com.sina.weibo.sdk.WeiboAppManager;
+
 import com.sina.weibo.sdk.auth.AccessTokenKeeper;
 import com.sina.weibo.sdk.auth.AuthInfo;
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.auth.WbAuthListener;
 import com.sina.weibo.sdk.auth.WbConnectErrorMessage;
 import com.sina.weibo.sdk.auth.sso.SsoHandler;
-import com.sina.weibo.sdk.constant.WBConstants;
-import com.sina.weibo.sdk.statistic.WBAgent;
-import com.sina.weibo.sdk.utils.WbUtils;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class WBDemoMainActivity extends Activity {
 
     private static Oauth2AccessToken mAccessToken;
     private TextView tvShowToken;
+    private TextView tvShowUser;
     private SsoHandler mSsoHandler;
 
     @Override
@@ -31,21 +37,35 @@ public class WBDemoMainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvShowToken = (TextView) findViewById(R.id.tv_show_token);
+        tvShowUser = (TextView) findViewById(R.id.tv_show_userMessage);
 
-        WbSdk.install(this, new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE));
+        WbSdk.install(getApplicationContext(), new AuthInfo(getApplicationContext(), Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE));
 
         mSsoHandler = new SsoHandler(this);
 
         mAccessToken = AccessTokenKeeper.readAccessToken(this);
         if (mAccessToken.isSessionValid()) {
-            tvShowToken.setText(mAccessToken.toString());
+            updateTokenView(true);
 
         } else {
             auth2AllInOne();
         }
     }
 
-    private void auth2AllInOne(){
+    private void updateTokenView(boolean hasExisted) {
+        String date = new SimpleDateFormat("yyyy/MM/dd HH.mm.ss").format(new java.util.Date(mAccessToken.getExpiresTime()));
+        String format = getString(R.string.weibosdk_demo_token_to_string_format_1);
+        tvShowToken.setText(String.format(format, mAccessToken.getToken(), date));
+
+        String message = String.format(format, mAccessToken.getToken(), date);
+        if (hasExisted) {
+            message = getString(R.string.weibosdk_demo_token_has_existed) + "\n" + message;
+        }
+        tvShowToken.setText(message);
+
+    }
+
+    private void auth2AllInOne() {
         mSsoHandler.authorize(new WbAuthListener() {
             @Override
             public void onSuccess(final Oauth2AccessToken oauth2AccessToken) {
@@ -53,9 +73,13 @@ public class WBDemoMainActivity extends Activity {
                     @Override
                     public void run() {
                         mAccessToken = oauth2AccessToken;
-                        AccessTokenKeeper.writeAccessToken(WBDemoMainActivity.this, mAccessToken);
-                        Toast.makeText(WBDemoMainActivity.this, "授权成功", Toast.LENGTH_SHORT).show();
-                        tvShowToken.setText(mAccessToken.toString());
+                        if (mAccessToken.isSessionValid()) {
+                            updateTokenView(false);
+                            AccessTokenKeeper.writeAccessToken(WBDemoMainActivity.this, mAccessToken);
+                            Toast.makeText(WBDemoMainActivity.this, "授权成功", Toast.LENGTH_SHORT).show();
+
+                        }
+
                     }
                 });
 
@@ -73,6 +97,37 @@ public class WBDemoMainActivity extends Activity {
 
             }
         });
+
+    }
+
+    public void getUserMessages(View view) {
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder().get().url("https://api.weibo.com/2/users/show.json?access_token=" + mAccessToken.getToken() + "&uid=" + mAccessToken.getUid()).build();
+        new Thread() {
+            @Override
+            public void run() {
+
+
+                try {
+                    final Response response = client.newCall(request).execute();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                tvShowUser.setText(response.body().string());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
+
 
     }
 
---
+++ app/src/main/java/com/example/administrator/lssz/beans/UserBean.java
@@ -0,0 +1,8 @@
+package com.example.administrator.lssz.beans;
+
+/**
+ * Created by Administrator on 2018/3/19.
+ */
+
+public class UserBean  {
+}
